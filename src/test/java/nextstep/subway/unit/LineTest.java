232923cb027974bef8c61f8b77b34f0dadaacbfd
danny <gryb809@gmail.com>
2024-07-16 22:50:48 +0900

    refactor: 지하철 구간 생성 중복 로직 통합

--- src/test/java/nextstep/subway/unit/LineTest.java
+++ src/test/java/nextstep/subway/unit/LineTest.java
@@ -13,27 +13,28 @@ class LineTest {
     private Station 강남역;
     private Station 신논현역;
     private Line 신분당선;
+    private Section 새로운구간;
 
     @BeforeEach
     void setup() {
         강남역 = new Station("강남역");
         신논현역 = new Station("신논현역");
         신분당선 = new Line("신분당선", "bg-red-600");
+        새로운구간 = Section.createSection(
+                신분당선,
+                강남역,
+                신논현역,
+                1
+        );
     }
 
     @Test
     void addSection() {
         // given
         Sections 신분당선구간 = 신분당선.getSections();
-        Section newSection = Section.createSection(
-                신분당선,
-                강남역,
-                신논현역,
-                1
-        );
 
         // when
-        신분당선구간.addSections(newSection);
+        신분당선구간.addSections(새로운구간);
 
         // then
         assertThat(신분당선구간.size()).isEqualTo(1);
@@ -41,20 +42,16 @@ class LineTest {
 
     @Test
     void getStations() {
-
+        // given
+        Sections 신분당선구간 = 신분당선.getSections();
+        신분당선구간.addSections(새로운구간);
     }
 
     @Test
     void removeSection() {
         // given
         Sections 신분당선구간 = 신분당선.getSections();
-        Section newSection = Section.createSection(
-                신분당선,
-                강남역,
-                신논현역,
-                1
-        );
-        신분당선구간.addSections(newSection);
+        신분당선구간.addSections(새로운구간);
 
         // when
         신분당선구간.deleteLastSection();
